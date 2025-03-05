7ec973d52feb676c40e6aba9489256d483fd59ad
YaricDunaev <yaricdunaev0@gmail.com>
2025-03-05 17:30:07 +0300

    Add files via upload

--- scripts/cards.js
+++ scripts/cards.js
@@ -24,3 +24,40 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+
+
+const cardCont = document.querySelector('.places__list');
+
+const imagePopup = document.querySelector('.popup_type_image');
+const imageLink = imagePopup.querySelector('.popup__image');
+const captionText = imagePopup.querySelector('.popup__caption');
+
+function createCard(card_name, card_link) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card_link;
+  cardElement.querySelector('.card__title').textContent = card_name;
+
+  cardElement.querySelector('.card__like-button').addEventListener('click', function(event){
+    console.log(event.target);
+    event.target.classList.toggle('card__like-button_is-active');
+  });
+
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function(event){
+    event.target.closest('.card').remove();
+
+  });
+
+  cardElement.querySelector('.card__image').addEventListener('click', function(event){
+    openModal(imagePopup);
+    imageLink.src = card_link;
+    captionText.textContent = card_name;
+  })
+
+  return cardElement;
+}
+
+initialCards.forEach(function (card) {
+  cardCont.append(createCard(card.name, card.link));
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,86 @@
+const profilePopup = document.querySelector('.popup_type_edit');
+const cardPopup = document.querySelector('.popup_type_new-card');
+
+
+function openModal(popup) {      
+    popup.classList.add('popup_is-opened');
+}
+
+function closeModal(popup){
+    popup.classList.remove('popup_is-opened');
+}
+
+const profilePopupButtonOpen = document.querySelector('.profile__edit-button');
+profilePopupButtonOpen.addEventListener('click', function() {
+    openModal(profilePopup);
+    const profileInputTitle = document.querySelector('.popup__input_type_name');
+    const profileInputDescription = document.querySelector('.popup__input_type_description');
+    profileInputTitle.value = document.querySelector('.profile__title').textContent;
+    profileInputDescription.value = document.querySelector('.profile__description').textContent;
+});
+
+function handleProfileFormSubmit(evt) {
+    //console.log('функция начала работу')
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+    const profileTitle = document.querySelector('.profile__title');
+    const profileDescription = document.querySelector('.profile__description');
+    //console.log('элементы выбраны')
+    profileTitle.textContent = document.querySelector('.popup__input_type_name').value;
+    profileDescription.textContent = document.querySelector('.popup__input_type_description').value;
+    closeModal(profilePopup); 
+    //console.log('элементы вставлены')
+}
+
+const profileFormElement = profilePopup.querySelector('.popup__button');
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+profileFormElement.addEventListener('click', handleProfileFormSubmit); 
+
+const profilePopupButtonClose = profilePopup.querySelector('.popup__close');
+profilePopupButtonClose.addEventListener('click', function() {
+    closeModal(profilePopup);
+    //console.log('closeModal');
+});
+
+
+const cardPopupButtonOpen =document.querySelector('.profile__add-button');
+cardPopupButtonOpen.addEventListener('click', function() {
+    openModal(cardPopup);
+    const cardNameInput = document.querySelector('.popup__input_type_card-name');
+    const cardLinkInput = document.querySelector('.popup__input_type_url');
+})
+
+function handleCardFormSubmit(evt) {
+    //console.log('функция начала работу')
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+    const cardInputName = document.querySelector('.popup__input_type_card-name');
+    const cardInputUrl = document.querySelector('.popup__input_type_url');
+    console.log('элементы выбраны')
+    cardCont.prepend(createCard(cardInputName.value, cardInputUrl.value));
+
+    closeModal(cardPopup); 
+    //console.log('элементы вставлены')
+}
+
+const cardFormElement = cardPopup.querySelector('.popup__button');
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+cardFormElement.addEventListener('click', handleCardFormSubmit); 
+
+const cardPopupButtonClose = cardPopup.querySelector('.popup__close');
+cardPopupButtonClose.addEventListener('click', function() {
+    closeModal(cardPopup);
+    //console.log('closeModal');
+});
+
+imagePopup.querySelector('.popup__close').addEventListener('click', function(){
+    closeModal(imagePopup);
+});
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
